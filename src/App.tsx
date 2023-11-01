94a1118f0bcfdb0eb4e61b3a159ee0b047d28a06
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-01 17:02:59 +0100

    witih images

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import employees from './data/employees.json';
+import employees from "./data/employees.json";
 
 function App() {
-	const title = 'Employee Directory';
-	const introText = "Welcome to our Employee Directory, your one-stop destination for discovering and connecting with the talented individuals who make our organization thrive.";
+	const title = "Employee Directory";
+	const introText =
+		"Welcome to our Employee Directory, your one-stop destination for discovering and connecting with the talented individuals who make our organization thrive.";
 
 	return (
 		<>
 			<h1 className="text-3xl mb-3 text-blue-800">{title}</h1>
 			<p>{introText}</p>
-			<ul>
-				{employees.map(employee => {
+			<div>
+				{employees.map((employee) => {
 					return (
-						<li>{employee.firstName}</li>
-					)
+						<div className="bg-slate-800 mt-3 text-slate-200 p-3 rounded">
+							<div className="flex gap-3">
+								<img
+									className="w-28 rounded"
+									src={`images/employees/employee_${employee.employeeID}.jpg`}
+								/>
+								<div>
+									<div>
+										{employee.firstName} {employee.lastName}
+									</div>
+									<div className="text-yellow-200">
+										{employee.title}
+									</div>
+								</div>
+							</div>
+							<div className="mt-2 italic text-slate-400">
+								{employee.notes}
+							</div>
+						</div>
+					);
 				})}
-			</ul>
+			</div>
 		</>
 	);
 }
