a51fd4b3839ef7506b68c3b1519501af0ef85016
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-01 16:42:47 +0100

    basic info in

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-function App() {
+import employees from './data/employees.json';
 
+function App() {
 	const title = 'Employee Directory';
 	const introText = "Welcome to our Employee Directory, your one-stop destination for discovering and connecting with the talented individuals who make our organization thrive.";
 
@@ -7,6 +8,9 @@ function App() {
 		<>
 			<h1 className="text-3xl mb-3 text-blue-800">{title}</h1>
 			<p>{introText}</p>
+			<ul>
+				{employees.}
+			</ul>
 		</>
 	);
 }
